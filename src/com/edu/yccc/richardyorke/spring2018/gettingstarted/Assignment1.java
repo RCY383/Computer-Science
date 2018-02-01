d519003b62a8660afabb1fe0b70bc75022f39141
srichardyorke <srichardyorke@C218-1762.yccc.edu>
2018-01-31 19:26:34 -0500

    First Draft for Assignment 1

---
+++ src/com/edu/yccc/richardyorke/spring2018/gettingstarted/Assignment1.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.edu.yccc.richardyorke.spring2018.gettingstarted;
+
+/**
+ * @author Richard Yorke
+ * Spring 2018 semester
+ * CIS 174- Algorithms in Programming
+ * 
+ */
+
+// The 4 pillars of Object-Oriented Programming are Abstraction, Encapsulation, Inheritance, and Polymorphism.
+// I got these definitions from Building Java Programs Third Edition by Stuart Reges and Marty Stepp
+
+// 1. Abstraction means that focusing on essentials properties rather than innter details.
+// 2. Encapsulation is hiding the implementation details of an object from the clients of the object.
+// 3. Inheritance is a programming technique that allows a derived class to extend the functionality of a base class, inheriting all of its state and behavior.
+// 4. Polymorphism is the ability for the same code to be used with several different types of objects and for the code to behave differently depending on the actual type of object used.
+
+public class Assignment1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
