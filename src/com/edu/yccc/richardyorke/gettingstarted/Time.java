c1a9aec3c8e4def9c0a3a8ab1fc3455aaee114c0
richard <srichardyorke@yccc.edu>
2018-02-06 06:26:40 -0500

    An example of String concatenation

---
+++ src/com/edu/yccc/richardyorke/gettingstarted/Time.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.edu.yccc.richardyorke.gettingstarted;
+
+/**
+ * @author Richard Yorke
+ *
+ */
+public class Time {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int hours = 365 * 24;
+		int minutes = hours * 60;
+		int seconds = minutes * 60;
+		System.out.println("Hours in a year = " + hours);
+		System.out.println("Minutes in a year = " + minutes);
+		System.out.println("Seconds in a year = " + seconds);
+	}
+
+}
