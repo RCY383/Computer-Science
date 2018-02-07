58dd90096d4efdd9016708c3629fd8876d73b1ad
richard <srichardyorke@yccc.edu>
2018-02-07 08:36:51 -0500

    This is from Chapter 2 from Building Java Programs.

--- src/com/edu/yccc/richardyorke/gettingstarted/WriteSquares2.java
+++ src/com/edu/yccc/richardyorke/gettingstarted/WriteSquares2.java
@@ -5,14 +5,17 @@ package com.edu.yccc.richardyorke.gettingstarted;
 
 /**
  * @author Richard Yorke
- *
+ * Spring 2018 semester
+ * CIS 174- Algorithms in Programming
+ * Instructor: Michael Bourgeious
  */
+
+//In this example, I am using For loops to avoid repreating the same thing.
 public class WriteSquares2 {
 
-	/*
-	 */
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i + " squared = " + (i * i));
