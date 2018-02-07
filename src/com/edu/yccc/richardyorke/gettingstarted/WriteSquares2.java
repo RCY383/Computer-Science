57333643536e192a83c77e5e3b223998fa013c9e
richard <srichardyorke@yccc.edu>
2018-02-07 05:09:05 -0500

    This is for loop example.

---
+++ src/com/edu/yccc/richardyorke/gettingstarted/WriteSquares2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.edu.yccc.richardyorke.gettingstarted;
+
+/**
+ * @author Richard Yorke
+ *
+ */
+public class WriteSquares2 {
+
+	/*
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i + " squared = " + (i * i));
+		}
+	}
+
+}
