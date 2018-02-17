ec5e885d4fbc17b7e850f4895e89592c6f88ef07
richard <srichardyorke@yccc.edu>
2018-02-17 05:49:23 -0500

    This is my beginning draft of my first Project of CIS 174.

---
+++ src/com/edu/yccc/richardyorke/project1/JavaExam.java
@@ -0,0 +1,36 @@
+package com.edu.yccc.richardyorke.project1;
+
+import java.util.*;
+
+
+
+/**
+ * @author Richard Yorke
+ * Spring 2018 semester
+ * CIS 174- Algorithms in Programming
+ * Instructor: Michael Bourgeious
+ */
+public class JavaExam {
+
+
+	
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.print("Enter your first name: ");
+		Scanner console = new Scanner(System.in);
+		console.nextLine();
+		System.out.println();
+		
+		System.out.print("Enter your last name: ");
+		console.nextLine();
+		System.out.println();
+		System.out.print("Enter your student ID Number: ");
+		console.nextLine();
+		System.out.println();
+		
+		
+	
+	}
+	
+
+}
