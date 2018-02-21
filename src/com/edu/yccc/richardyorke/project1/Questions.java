4fb396a939881549add8eaae19dff1c7ac21c324
srichardyorke <srichardyorke@C218-1762.yccc.edu>
2018-02-21 17:40:42 -0500

    Testing

---
+++ src/com/edu/yccc/richardyorke/project1/Questions.java
@@ -0,0 +1,19 @@
+package com.edu.yccc.richardyorke.project1;
+
+public class Questions {
+
+	String questions;
+
+	public String getQuestions () {
+		return questions;
+	}
+	
+	public void setQuestions (String questions) {
+		 this.questions = questions;
+	}
+		
+	}
+
+
+
+
