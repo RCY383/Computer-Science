9d6389d22669962d5387257182b21bd06da7379a
richard <srichardyorke@yccc.edu>
2018-02-22 08:12:02 -0500

    I made an Answers class just like Questions class.

---
+++ src/com/edu/yccc/richardyorke/project1/Answers.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.edu.yccc.richardyorke.project1;
+
+/**
+ * @author Richard Yorke
+ * Spring 2018 semester
+ * CIS 174- Algorithms in Programming
+ * Instructor: Michael Bourgeious
+ */
+public class Answers {
+
+	String answers;
+
+	public String getAnswers () {
+		return answers;
+	}
+
+	public void setAnswers (String answers) {
+		this.answers = answers;
+	}
+
+}
+
+
+
