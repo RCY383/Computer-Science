7be4528bf4805de06b761d436a5c3e51e4743801
Richard Yorke <srichardyorke@yccc.edu>
2018-03-16 03:19:49 -0400

    This is the beginning of my second project Interface.

---
+++ src/com/edu/yccc/richardyorke/ExamInterface/Exams.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.edu.yccc.richardyorke.ExamInterface;
+
+/**
+ * @author Richard Yorke
+ * CIS 174-Algorthrims  in Programming
+ * Spring 2018
+ * Michael Bourgeois
+ *
+ */
+public interface Exams {
+	public void TakeExam();
+	public void questions();
+	public void answers();
+	public void calculateScore();
+	public void ExamResults();
+
+}
